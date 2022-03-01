4e6c248cc9515bfe2b96a599312b94ead99df8be
shyvesCCD <pedroochavess97@gmail.com>
2022-03-01 18:02:23 -0300

    Implementa a decifração.

--- Projeto1.py
+++ Projeto1.py
@@ -2,38 +2,48 @@ import numpy as np
 import string
 from assets import alphabet
 
-plaintext = "Eu gosto de banana"
-            #eugostodebanana
-            #chaveschavescha
+plaintext = input("Escreva o sua mensagem: ")
+key = input("Insira a sua chave: ")
 
 cipher_text = ""
+decoded_text = ""
 
 plaintext_filtered = plaintext.replace(" ", "").lower()
 
-plaintext_array = np.array(list(plaintext_filtered))
 key = np.array(list("chaves"))
 
 keys_list = list(alphabet)
 
-while len(plaintext_array) >= 1:
-
-  
+while len(plaintext_filtered) >= 1:
   # dar match da key com o plaintext (deixar mesmo tamanho) 
   for i in range(len(key)):
-    print("p_l", plaintext_array[0])
-    print("k_l", key[i])
 
-    plaintext_alphabet_value = alphabet.get(plaintext_array[0]) # e - 5
+    plaintext_alphabet_value = alphabet.get(plaintext_filtered[0]) # e - 5
     key_alphabet_value = alphabet.get(key[i]) # c - 3
 
-    print(plaintext_alphabet_value, key_alphabet_value)
-    cipher_alphabet_value = (plaintext_alphabet_value+key_alphabet_value) % 26 # 6
+    cipher_alphabet_value = (plaintext_alphabet_value + key_alphabet_value) % 26 # 6
     cipher_text = cipher_text + keys_list[cipher_alphabet_value]
-    print('key_value', keys_list[cipher_alphabet_value])
 
-     
-    plaintext_array = np.delete(plaintext_array,0)
-    if len(plaintext_array) == 0:
+    plaintext_filtered = plaintext_filtered[1:]
+
+    if len(plaintext_filtered) == 0:
       break
+
 print(cipher_text.upper())
 
+while len(cipher_text) > 1:
+    for i in range(len(key)):
+        cipher_text_alphabet_value = alphabet.get(cipher_text[0]) # g - 6
+        key_alphabet_value = alphabet.get(key[i]) # c - 2
+        
+        #Aqui pegamos o valor da letra para descriptografar a mensagem.
+        decoded_text_value = (cipher_text_alphabet_value - key_alphabet_value) % 26
+        #Começamos a montar a nossa string decriptografada.
+        decoded_text = decoded_text + keys_list[decoded_text_value]
+
+        cipher_text = cipher_text[1:]
+        
+        if (len(cipher_text)) == 0:
+            break;
+
+print(decoded_text)
