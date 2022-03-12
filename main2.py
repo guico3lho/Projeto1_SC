1a70ae1726e9e3acc498aaf0aa1e6f572e5b256b
GuiCoelhoDev <guiminervino@gmail.com>
2022-03-11 22:17:03 -0300

    Cria tabela com a porcentagem de frequencia das letras

--- main2.py
+++ main2.py
@@ -1,5 +1,6 @@
 from assets import desafio_1, desafio_1_teste, lf_english
 from functions_1 import filter
+import string
 
 
 ##### ABREVIAÇÕES IMPORTANTES PARA ENTENDER O CÓDIGO #####
@@ -16,46 +17,61 @@ from functions_1 import filter
 # while len(desafio_1) >= 1:
 
 
-
-desafio_1_filtered = filter(desafio_1).upper() # RVGLLAKIEGTY - ciphertext without numbers and ponctuations
+# RVGLLAKIEGTY - ciphertext without numbers and ponctuations
+desafio_1_filtered = filter(desafio_1).upper()
 
 print(desafio_1_filtered)
 
-key_size = 5 # size of the key used to decode
+key_size = 5  # size of the key used to decode
 
-dict_of_dicts = {} # dictionary that holds ${iterator} dictionaries
+dict_of_dicts = {}  # dictionary that holds ${iterator} dictionaries
+
+dict_of_dicts_frequency = {}
 
 
-iterator = 0 # iterator to pick frequency of letters
+iterator = 0  # iterator to pick frequency of letters
 while iterator < key_size:
-       
-    dict_of_lf_ciphertext = {}
-    for i in range(iterator, len(desafio_1_filtered),key_size): # percorre o ciphertext de ${key} em ${key} até o final. Faz isso para L1,L2,...,LN, sendo N = key_size
-        dict_of_lf_ciphertext_key = desafio_1_filtered[i] # R, A, ...
 
-        # Caso a letra encontrada não esteja no dicionário ainda
-        if dict_of_lf_ciphertext.get(dict_of_lf_ciphertext_key) == None: 
-            dict_of_lf_ciphertext[dict_of_lf_ciphertext_key] = 1
-            
-        # Se já está no dicionário, incrementa ocorrência em uma vez
-        else:          
-            dict_of_lf_ciphertext[dict_of_lf_ciphertext_key] = dict_of_lf_ciphertext.get(dict_of_lf_ciphertext_key) + 1
+    dict_of_lf_ciphertext = dict.fromkeys(string.ascii_uppercase, 0)
+    dict_of_lf_ciphertext_freq = dict.fromkeys(string.ascii_uppercase, 0)
+    # percorre o ciphertext de ${key} em ${key} até o final. Faz isso para L1,L2,...,LN, sendo N = key_size
+    for i in range(iterator, len(desafio_1_filtered), key_size):
+        dict_of_lf_ciphertext_key = desafio_1_filtered[i]  # R, A, ...
+
+        # caso encontre a letra, aumenta a sua quantidade no dicionário
+        dict_of_lf_ciphertext[dict_of_lf_ciphertext_key] = dict_of_lf_ciphertext.get(
+            dict_of_lf_ciphertext_key) + 1
 
     # adiciona o dicionário criado no dicionario de dicionarios
     dict_of_dicts_key = 'H' + str(iterator)
-    
-    #ordena o dict alfabeticamente para auxiliar no processo de descobrir as letras da chave
+
+    # ordena o dict alfabeticamente para auxiliar no processo de descobrir as letras da chave
     dict_of_lf_ciphertext = dict(sorted(dict_of_lf_ciphertext.items()))
+
+    # armazenar o numero total de letras
+    total = 0
+    for letter, quantity in dict_of_lf_ciphertext.items():
+        total += quantity
+    # pegar a ocorrencia e dividir pelo total
+    dict_of_dicts_frequency_key = 'F' + str(iterator)
+
+    for (letter, frequency), (letter2, quantity) in zip(dict_of_lf_ciphertext_freq.items(), dict_of_lf_ciphertext.items()):
+        frequency = round(quantity*100/total, 2)
+        # print(letter, frequency, letter2, quantity)
+        dict_of_lf_ciphertext_freq[letter] = frequency
+
+    # print(dict_of_lf_ciphertext_freq)
+    # dict_of_dicts_frequency[dict_of_dicts_frequency_key] =
+    dict_of_dicts_frequency[dict_of_dicts_frequency_key] = dict_of_lf_ciphertext_freq
     dict_of_dicts[dict_of_dicts_key] = dict_of_lf_ciphertext
-    iterator+=1
+    iterator += 1
+
 
 # printa os dicionarios de frequencia para cada letra da chave desconhecida
-for letter_frequency_dict in dict_of_dicts.items(): 
-    print(letter_frequency_dict) # tuple
+for letter_frequency_dict, letter_frequency_dict_freq in zip(dict_of_dicts.items(), dict_of_dicts_frequency.items()):
+    print("\nTable quantity", letter_frequency_dict)  # tuple
+    print("\nTable frequency", letter_frequency_dict_freq)
 
 # ordena o dict de lf em ingles
 lf_english = dict(sorted(lf_english.items()))
-print(lf_english)
-
-
-
+# print(lf_english)
